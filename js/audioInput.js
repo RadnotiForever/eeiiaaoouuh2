9a5e2cdee76d0d5660dfaced96f498456f078bcb
Balint Kovacs <dratini0@gmail.com>
2017-01-28 16:14:20 +0000

    Added some audio input

---
+++ js/audioInput.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/**
+ * Created by bala on 1/28/17.
+ */
+
+var AUDIO_INPUT_STATUS_LOADING = 0;
+var AUDIO_INPUT_STATUS_UNSUPPORTED = 1;
+var AUDIO_INPUT_STATUS_REQUESTED = 2;
+var AUDIO_INPUT_STATUS_WORKING = 3;
+var AUDIO_INPUT_STATUS_READY = 4;
+
+var audioInputState = {
+    status: AUDIO_INPUT_STATUS_LOADING,
+    valid: false,
+    lastRead: 0,
+    confidence: 100,
+    frequency: 440
+}
+
+function audioInputError(){
+    audioInputState.status = AUDIO_INPUT_STATUS_UNSUPPORTED;
+    throw "Some features are missing from your browser";
+    return false;
+};
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext || audioInputError();
+if (!navigator.getUserMedia)
+    navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || audioInputError();
+
+function updateAudioInput() {
+    return {
+        status: AUDIO_INPUT_STATUS_LOADING,
+        valid: false,
+        lastRead: 0,
+        confidence: 100,
+        frequency: 440
+    }
+}
